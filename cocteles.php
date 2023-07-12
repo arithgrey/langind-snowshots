9db1a595c4f25e597250a7de5df4715d8ee729b6
arihgrey@gmail.com <arihgrey@gmail.com>
2023-07-12 02:34:49 -0600

    [FEAT] pixel

--- cocteles.php
+++ cocteles.php
@@ -33,7 +33,8 @@
 
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
 
-    <script>
+     <!-- Meta Pixel Code Piñatas-->
+     <script>
         ! function(f, b, e, v, n, t, s) {
             if (f.fbq) return;
             n = f.fbq = function() {
@@ -52,11 +53,12 @@
             s.parentNode.insertBefore(t, s)
         }(window, document, 'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '603499584596714');
+        fbq('init', '192996083728690');
         fbq('track', 'PageView');
     </script>
-    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=603499584596714&ev=PageView&noscript=1" /></noscript>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=192996083728690&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
+    
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-DWKGKK30KK"></script>
