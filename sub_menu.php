69e335d825f4ae12ce4ca38b64ffa07c27bb4be4
arihgrey@gmail.com <arihgrey@gmail.com>
2023-08-10 02:44:52 -0600

    [chore]

--- sub_menu.php
+++ sub_menu.php
@@ -1,15 +1,20 @@
 <div>
-    <a class="display-4" href="taquizas.php">
+    <a class="text-menu" href="taquizas.php">
         Taquizas
         <span class="effectHover"></span></a>
 </div>
 <div>
-    <a class="display-4" href="parrilladas.php">
+    <a class="text-menu" href="parrilladas.php">
         Parrilladas
         <span class="effectHover"></span></a>
 </div>
 <div>
-    <a class="display-4" href="cocteles.php">
+    <a class="text-menu" href="cocteles.php">
         Cócteles
         <span class="effectHover"></span></a>
 </div>
+<style>
+    .text-menu{
+        font-size: 1.8em!important;
+    }
+</style>
